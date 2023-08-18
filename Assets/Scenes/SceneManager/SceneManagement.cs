cb697e61878ae523c4148f4694b1c5e3a390d3d9
Mitchell Opitz <mitchell@mitchellopitz.net>
2023-08-18 12:30:42 -0500

    feat - Scene setup and fader

--- Assets/Scenes/SceneManager/SceneManagement.cs
+++ Assets/Scenes/SceneManager/SceneManagement.cs
@@ -1,9 +1,11 @@
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using System.Collections;
 
 public class SceneManagement : MonoBehaviour
 {
     public static SceneManagement instance;
+    public Fader fader; // Reference to the Fader script
 
     private void Awake()
     {
@@ -18,6 +20,13 @@ public class SceneManagement : MonoBehaviour
 
     public void LoadSceneByName(string sceneName)
     {
+        fader.FadeOut();
+        StartCoroutine(LoadSceneWithDelay(sceneName));
+    }
+
+    private IEnumerator LoadSceneWithDelay(string sceneName)
+    {
+        yield return new WaitForSeconds(fader.fadeDuration); // Wait for fade-out to complete
         SceneManager.LoadScene(sceneName);
     }
 
