588ac5e78568f50c2dcb65ed8706533e5c3ccc1b
Mitchell Opitz <mitchell@mitchellopitz.net>
2023-08-18 03:18:23 -0500

    feat - Scene manager

---
+++ Assets/Scenes/SceneManager/SceneManagement.cs
@@ -0,0 +1,28 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneManagement : MonoBehaviour
+{
+    public static SceneManagement instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    public void LoadSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
